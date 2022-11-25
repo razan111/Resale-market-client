464abd89102d545dd61736bc3d88acf3bd8d634b
razan111 <razan15-12966@diu.edu.bd>
2022-11-25 21:47:27 +0600

    show my product

--- src/Pages/Dashboard/MyProduct/MyProduct.js
+++ src/Pages/Dashboard/MyProduct/MyProduct.js
@@ -26,6 +26,37 @@ const MyProduct = () => {
     return (
         <div>
             <h2>My Product: {products?.length}</h2>
+
+
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+
+                {
+                    products.map(product => <div className="card  bg-base-100 shadow-xl">
+                    <figure><img className='h-80 w-full object-cover' src={product.image} alt="Shoes" /></figure>
+                    <div className="card-body">
+                      <h2 className="card-title">
+                        {product.productName}
+                        <div className="badge badge-secondary">{product.conditionType}</div>
+                      </h2>
+                      <p>If a dog chews shoes whose shoes does he choose?</p>
+
+                      <div className="card-actions justify-between">
+                        <h2>Price: {product.price}</h2>
+                        <h2>OldPrice: {product.oldPrice}</h2>
+                        <h2>Location: {product.location}</h2>
+                        <h2>Category: {product.productCategory}</h2>
+                        <h2>Purchase: {product.purchase}</h2>
+                        <h2>Number: {product.number}</h2>
+                      </div>
+                      <div className="card-actions justify-end">
+                        <div className="badge badge-outline">Fashion</div> 
+                        <div className="badge badge-outline">Products</div>
+                      </div>
+                    </div>
+                  </div>)
+                }
+
+            </div>
         </div>
     );
 };
